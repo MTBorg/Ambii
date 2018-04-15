387080f01bf4586084a3849e7e1fd4255e5698a4
Martin Terneborg <martinterneborg96@gmail.com>
2018-04-15 20:24:51 +0200

    Added a vector to hold all currently selected monitors and a constructor to initialize it.

--- sln/Ambii/include/SetupGUI.h
+++ sln/Ambii/include/SetupGUI.h
@@ -15,11 +15,14 @@
 */
 class SetupGUI {
 private:
-	static VOID DrawMonitor(CONST HDC& hdc, CONST Monitor& monitor, CONST UINT x, CONST UINT y, CONST UINT width, CONST UINT height);
+	CONST std::vector<Monitor>& m_selectedMonitors;
+	VOID DrawMonitor(CONST HDC& hdc, CONST Monitor& monitor, CONST UINT x, CONST UINT y, CONST UINT width, CONST UINT height);
 public:
-	static VOID Draw(CONST HWND hWndParent, CONST std::vector<MonitorTab>& monitorTabs);
-	static HWND Create(CONST HWND hWndParent, CONST UINT x, CONST UINT y, CONST UINT width, CONST UINT height,
-		CONST std::vector<Monitor>& selectedMonitors);
+	//Default constructor
+	SetupGUI(CONST std::vector<Monitor>& m_selectedMonitors);
+
+	VOID Draw(CONST HWND hWndParent, CONST std::vector<MonitorTab>& monitorTabs);
+	HWND Create(CONST HWND hWndParent, CONST UINT x, CONST UINT y, CONST UINT width, CONST UINT height);
 };
 
 #endif
