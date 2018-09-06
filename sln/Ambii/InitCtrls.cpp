544e71a26a8f7e235b3747b04ea59b8945f6ed6c
Martin Terneborg <martinterneborg96@gmail.com>
2018-09-06 19:00:53 +0200

    Copied over some old function protypes and definitions from MonitorTab.cpp/MonitorTab.h for initilization of text, edit and checkbox(buttons) controls

--- sln/Ambii/InitCtrls.cpp
+++ sln/Ambii/InitCtrls.cpp
@@ -3,3 +3,72 @@
 */
 
 #include "InitCtrls.h"
+
+/*
+	//TODO: Comment
+*/
+CONST HWND InitTextCtrl(CONST HWND hWndParent, CONST LPCWSTR text, CONST UINT x, CONST UINT y) {
+	//Create a DC in memory to hold the font
+	HDC hdcMem = CreateCompatibleDC(NULL);
+	SelectObject(hdcMem, (HBRUSH)GetStockObject(DEFAULT_GUI_FONT));
+	SIZE size;
+	GetTextExtentPoint32(hdcMem, EDITTEXT_POSITION_BOTTOM, lstrlen(EDITTEXT_POSITION_BOTTOM), &size);
+	DeleteDC(hdcMem);
+
+	HWND hText = CreateWindow(
+		WC_STATIC,
+		text,
+		WS_CHILD | WS_VISIBLE,
+		x, y,
+		size.cx, size.cy,
+		hWndParent,
+		NULL,
+		GetModuleHandle(NULL),
+		NULL);
+	SendMessage(hText, WM_SETFONT, (WPARAM)GetStockObject(DEFAULT_GUI_FONT), NULL);
+
+	return hText;
+}
+
+/*
+	//TODO: Comment
+*/
+CONST HWND InitEditCtrl(CONST HWND hWndParent, CONST UINT x, CONST UINT y, CONST UINT width, CONST UINT height, CONST HMENU id) {
+	HWND hEdit = CreateWindow(
+		WC_EDIT, L"",
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER,
+		x, y,
+		width, height,
+		hWndParent,
+		id,
+		GetModuleHandle(NULL),
+		NULL);
+	SendMessage(hEdit, WM_SETFONT, (WPARAM)GetStockObject(DEFAULT_GUI_FONT), NULL);
+
+	return hEdit;
+}
+
+/*
+	TODO: Comment
+*/
+CONST HWND InitCheckboxCtrl(CONST HWND hWndParent, CONST UINT x, CONST UINT y, CONST LPCWSTR text, CONST HMENU id) {
+	//Create a DC in memory to hold the font
+	HDC hdcMem = CreateCompatibleDC(NULL);
+	SelectObject(hdcMem, (HBRUSH)GetStockObject(DEFAULT_GUI_FONT));
+	SIZE size;
+	GetTextExtentPoint32(hdcMem, EDITTEXT_POSITION_BOTTOM, lstrlen(EDITTEXT_POSITION_BOTTOM), &size);
+	DeleteDC(hdcMem);
+
+	HWND hCheckbox = CreateWindow(
+		WC_BUTTON, text,
+		WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX | BS_LEFTTEXT,
+		x, y,
+		size.cx + 2 * GetSystemMetrics(SM_CXMENUCHECK), size.cy,
+		hWndParent,
+		id,
+		GetModuleHandle(NULL),
+		NULL);
+	SendMessage(hCheckbox, WM_SETFONT, (WPARAM)GetStockObject(DEFAULT_GUI_FONT), NULL);
+
+	return hCheckbox;
+}
--- sln/Ambii/include/InitCtrls.h
+++ sln/Ambii/include/InitCtrls.h
@@ -7,4 +7,8 @@
 
 #include <Windows.h>
 
+CONST HWND InitTextCtrl(CONST HWND hWndParent, CONST LPCWSTR text, CONST UINT x, CONST UINT y);
+CONST HWND InitEditCtrl(CONST HWND hWndParent, CONST UINT x, CONST UINT y, CONST UINT width, CONST UINT height, CONST HMENU id);
+CONST HWND InitCheckboxCtrl(CONST HWND hWndParent, CONST UINT x, CONST UINT y, CONST LPCWSTR text, CONST HMENU id);
+
 #endif
